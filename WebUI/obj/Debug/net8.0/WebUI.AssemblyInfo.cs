70e35ec78616dfeb7856e2687a936d9d1ed6fb60
aryasuat <aryasuat@hotmail.com>
2024-10-15 21:23:19 +0300

    Last edit before error

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15553e4decdb3a02b92a6c571a0d8c1637805fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6bd3ae79a5780b5cfe7e21cdbb802b19cf2046")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
